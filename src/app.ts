0e173990129797aa6e3f1051cc4e74639730dbac
Sabbir Hossain <sabbirhossainrahad@gmail.com>
2023-05-30 22:17:14 +0600

    [DONE] setup express, mongoose, cors and body parser

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+const app: Application = express();
+
+// using cors
+app.use(cors());
+// using body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req: Request, res: Response) => {
+	res.send('Hello World!');
+});
+
+export default app;
